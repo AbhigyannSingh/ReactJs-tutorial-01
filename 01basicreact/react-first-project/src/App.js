24e2712dd22c27f204d6f2615c352326bc4a2097
Abhigyan <pratapabhi137@gmail.com>
2024-06-11 21:43:19 +0530

    first commit

---
+++ 01basicreact/react-first-project/src/App.js
@@ -0,0 +1,16 @@
+import Chai from './Chai.js'
+
+
+
+function App() {
+  return (
+    <>
+    <h1>
+      <Chai/>
+      hello there
+    </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const username = "chai with code"
+
+  return (
+    <> 
+        <h1>help me abhigyan</h1>
+        <p>abhigyan {username}</p>
+
+    </>
+
+  )
+}
+
+export default App
