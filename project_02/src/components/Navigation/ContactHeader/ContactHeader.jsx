65193ab8d76b78c3cc0feff6d34c15ea0225ef1e
Abhigyan <pratapabhi137@gmail.com>
2024-08-12 00:42:20 +0530

    m

--- project_02/src/components/Navigation/ContactHeader/ContactHeader.jsx
+++ project_02/src/components/Navigation/ContactHeader/ContactHeader.jsx
@@ -1,10 +1,10 @@
-import styles from './ContactHeader.css'
+import  './ContactHeader.css'
 
 const ContactHeader = () =>{
 
     return(
 
-        <div className={styles.contact_section}>
+        <div>
             <h1>Contact Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facere molestiae voluptatibus delectus, nihil eligendi? Nemo delectus nisi tempore adipisci aliquam illum deserunt aliquid dolorem ipsa eius suscipit, praesentium repudiandae natus architecto facere eveniet veniam obcaecati porro, alias placeat eligendi quaerat possimus nesciunt? Repellat vel eveniet recusandae delectus adipisci soluta!</p>
 
